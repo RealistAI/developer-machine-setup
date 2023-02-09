ea8f73412191bbf4f4f4b4eee8c5b48eac626879
Ryan den Otter <ryan.denotter@realist.ai>
2023-02-09 19:46:13 +0000

    Adding required steps to install vim plugins

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
 echo "Copying config files"
-cp ./config/* ~/.config
+cp -r ./config/* ~/.config
+
+# Install the vim plugin manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+# Install nodejs which is required for Coc
+sudo apt install -y nodejs
+
+# Install all of the plugins
+nvim --headless +PlugInstall +qall
+
