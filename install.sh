05401c5573e03cde97d61b73a579028ae15a908f
Ryan den Otter <ryan.denotter@realist.ai>
2023-02-09 20:55:04 +0000

    Trying to change the order around to see if I need to download the package manager after installing NodeJs

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 echo "Copying config files"
 cp -r ./config/* ~/.config/
 
-echo "#######################################################"
-echo "# Installing the vim plugin manager                   #"
-echo "#######################################################"
-# Install the vim plugin manager
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 # Install nodejs which is required for Coc
 echo "#######################################################"
 echo "# Installing Nodejs which is required for code        #"
@@ -17,6 +10,13 @@ sudo apt update
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt install -y nodejs
 echo "Installed nodejs $(node -v)"
+
+echo "#######################################################"
+echo "# Installing the vim plugin manager                   #"
+echo "#######################################################"
+# Install the vim plugin manager
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo "#######################################################"
 echo "# Executing PlugInstall to install all the plugins    #"
