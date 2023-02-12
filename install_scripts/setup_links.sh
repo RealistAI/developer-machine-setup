4ccfa7d692be602a8fa3dc7cc1af16a01abb7303
Ryan den Otter <ryan.denotter@realist.ai>
2023-02-12 00:21:48 +0000

    First step towards using lua

---
+++ install_scripts/setup_links.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -x
+
+# Check if there is already a .config file
+if [[ -d "$HOME/.config" ]]; then
+    MOVE_CONFIG=true
+fi
+
+# Back up any config files
+if [[ $MOVE_CONFIG ]]; then
+    echo "Moving config"
+    mv ~/.config ~/_temp_config
+fi
+
+# Create a symlink from our git repo to ~/.config
+ln -sv ~/git/config_manager/xdg_config/ ~/.config
+
+# Move the backed up configs back
+if [[ $MOVE_CONFIG ]]; then
+    cp -r ~/_temp_config/* ~/.config/
+    rm -rf ~/_temp_config
+fi
---
+++ xdg_config/nvim/init.lua
@@ -0,0 +1,25 @@
+-- Leader key is a space
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set up the globals
+-- See ./lua/globals
+require "globals.options"
+require "globals.remaps"
+
+-- See ./lua/lsp
+require "lsp.lsp"
+
+-- See ./lua/plugins
+require "plugins.packer-config"
+require "plugins.telescope-config"
+require "plugins.gruvbox-config"
+
+
+-- Automatically run PackerSync if the packer file changes.
+local group = vim.api.nvim_create_augroup("packer_user_config", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+	command = "source <afile> | PackerSync",
+	pattern = "packer.lua", -- the name of your plugins file
+	group = group,
+})
